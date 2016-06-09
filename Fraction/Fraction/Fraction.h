df846f6d10665e64fcbd290563dbedcc63f8db45
Andrew Edwards <aae16@zips.uakron.edu>
2016-06-08 20:15:32 -0400

    Initial implementation of Fraction class implementing Templates.

--- Fraction/Fraction/Fraction.h
+++ Fraction/Fraction/Fraction.h
@@ -25,35 +25,111 @@ FUTURE WORK:
 	Set Numerator/Denominator individual functions --Possible
 */
 
+template<typename T, typename U>
 class Fraction
 {
 public:
-	Fraction(int, int);
+	Fraction() {}
+	Fraction(T numerator, U denominator) : numerator_(numerator), denominator_(denominator) {}
 
-	Fraction operator* (Fraction const&);
-	Fraction operator/ (Fraction const&);
-	Fraction operator+ (Fraction const&);
-	Fraction operator- (Fraction const&);
-	Fraction operator= (Fraction const&);
-	bool operator== (Fraction const&);
-	bool operator!= (Fraction const&);
-	bool operator> (Fraction const&);
-	bool operator>= (Fraction const&);
-	bool operator< (Fraction const&);
-	bool operator<= (Fraction const&);
+	Fraction operator* (const Fraction&);
+	Fraction operator/ (const Fraction&);
+	Fraction operator+ (const Fraction&);
+	Fraction operator- (const Fraction&);
+	bool operator== (const Fraction&);
+	bool operator!= (const Fraction&);
+	bool operator> (const Fraction&);
+	bool operator>= (const Fraction&);
+	bool operator< (const Fraction&);
+	bool operator<= (const Fraction&);
 
-	double Evaluate() const; //Evaluates fraction (ie numerator / denominator)
-
-	int GetNumerator() const { return numerator_; }
-	int GetDenominator() const { return denominator_; }
+	void Display();
+	T GetNumerator() { return numerator_; }
+	T GetDenominator() { return denominator_; }
 
 private:
-	int numerator_;
-	int denominator_;
+	T numerator_;
+	U denominator_;
 
-	void validate(); //To Validate denominator is > 0 as well as calls reduce method
-	int lcm(int, int) const; //To find the Least Common Multiple of fractions, used for add/subtract as well as ordering
-	void reduce(); //Reduces fraction to lowest form (eg 4/8 -> 1/2). 
+	T lcm(T, U);
+	Fraction reduce(const Fraction&);
 };
+
+template<typename T, typename U>
+Fraction<T, U> Fraction<T, U>::operator* (const Fraction& frac) {
+	Fraction<T, U> temp;
+	temp.numerator_ = numerator_ * frac.numerator_;
+	temp.denominator_ = denominator_ * frac.denominator_;
+	return temp;
+}
+
+template<typename T, typename U>
+Fraction<T, U> Fraction<T, U>::operator/ (const Fraction& frac) {
+	Fraction<T, U> temp;
+	temp.numerator_ = numerator_ * frac.denominator_;
+	temp.denominator_ = denominator_ * frac.numerator_;
+	return temp;
+}
+
+template<typename T, typename U>
+Fraction<T, U> Fraction<T, U>::operator+ (const Fraction& frac) {
+	Fraction<T, U> temp;
+	T least = lcm(denominator_, frac.denominator_);
+	temp.numerator_ = (numerator_ * (least / denominator_)) + (frac.numerator_ * (least / frac.denominator_));
+	temp.denominator_ = least;
+	return reduce(temp);
+}
+
+template<typename T, typename U>
+Fraction<T, U> Fraction<T, U>::operator- (const Fraction& frac) {
+	Fraction<T, U> temp;
+	T least = lcm(denominator_, frac.denominator_);
+	temp.numerator_ = (numerator_ * (least / denominator_)) - (frac.numerator_ * (least / frac.denominator_));
+	temp.denominator_ = least;
+	return reduce(temp);
+}
+
+template<typename T, typename U>
+bool Fraction<T, U>::operator== (const Fraction& frac) {
+	return ((numerator_ / denominator_) == (frac.numerator_ / frac.denominator_));
+}
+
+template<typename T, typename U>
+void Fraction<T, U>::Display() {
+	cout << numerator_ << " / " << denominator_ << endl;
+}
+
+template<typename T, typename U>
+T Fraction<T, U>::lcm(T a, U b) {
+	T m, n;
+
+	m = a;
+	n = b;
+
+	while (m != n) {
+		if (m < n)
+			m = m + a;
+		else
+			n = n + b;
+	}
+
+	return m;
+}
+
+template<typename T, typename U>
+Fraction<T, U>  Fraction<T, U>::reduce(const Fraction& frac) {
+	T a = abs(frac.numerator_);
+	U b = abs(frac.denominator_);
+	T c = a % b;
+	while (c != 0)
+	{
+		a = b;
+		b = c;
+		c = a % b;
+	}
+
+	Fraction<T, U> temp((frac.numerator_ / b), (frac.denominator_ / b));
+	return temp;
+}
 
 #endif
--- Fraction/Fraction/Main.cpp
+++ Fraction/Fraction/Main.cpp
@@ -5,19 +5,6 @@ using namespace std;
 
 int main()
 {
-	Fraction frac(-4, -8);
-	Fraction frac2(1, -2);
-
-	frac = frac2;
-
-	cout << frac.GetNumerator() << " / " << frac.GetDenominator() << " = " << frac.Evaluate() << endl;
-
-	frac = frac * frac2;
-
-	double result = frac.Evaluate();
-
-	cout << frac.GetNumerator() << " / " << frac.GetDenominator() << " = " << result << endl;
-
 	int temp;
 	cin >> temp;
 
